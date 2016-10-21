5f73be96f5d75978ff8416bbc428d9af47ca55ba
sjcanvas <763715693@qq.com>
2016-10-21 09:45:13 +0800

    for testing fetch

--- coralsea.py
+++ coralsea.py
@@ -2,7 +2,6 @@
 # coding:utf-8 
    def step8(self):
         log.info('Calculate the percentage')
-        cluster = (float(self.cluster_count)/float(self.forword))*100
         self.cluster_detect_precent = round(cluster,3)
         distinct1 = (float(self.distinct_id_num)/float(self.url))*100
         self.distinct_id_precent = round(distinct1,3)        
