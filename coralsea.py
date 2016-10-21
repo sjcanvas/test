ec62f471fe8a6bd8b3f834c336deb47a46f5a65a
sjcanvas <763715693@qq.com>
2016-10-21 09:57:17 +0800

    for git fetch

--- coralsea.py
+++ coralsea.py
@@ -9,10 +9,3 @@
         return Testcase.PASS         
 
     def clean(self):
-
-        log.info('Calculate the percentage')
-        cluster = (float(self.cluster_count)/float(self.forword))*100
-        self.cluster_detect_precent = round(cluster,3)
-        distinct1 = (float(self.distinct_id_num)/float(self.url))*100
-        self.distinct_id_precent = round(distinct1,3)        
-        return Testcase.PASS 
